7ba7d4bf481211a5c2d3eb0ef61936e0ffe877b2
shxd <paolaccialesssndro@gmail.com>
2024-05-23 17:21:06 +0200

    epic fail

--- api/submissions.py
+++ api/submissions.py
@@ -1,14 +1,14 @@
 from utils import download_attachments
 
 
-async def handle_submissions(message, filetype, num, year):
+async def handle_submissions(message, file, num, year):
     attachments = message.attachments
     # TODO: Detect which comp, to limit number of files
     attachments = attachments[:2]
-    if filetype == "rkg":
+    if file == "rkg":
         file_name = f"Task{num}-{year}By{message.author.display_name}"
         await download_attachments(attachments, file_name)
-    elif filetype == "rksys":
+    elif file == "rksys":
         file_name = f"Task{num}-{year}By{message.author.display_name}_rksys"
         await download_attachments(attachments, file_name)
 
