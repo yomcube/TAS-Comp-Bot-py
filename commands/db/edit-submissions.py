233566ccffb120f85eb89ef462e1269ce6963979
DashQC <Megayoshi.remi@gmail.com>
2024-05-24 09:54:16 -0400

    Time format in edit-submissions has been fixed to m:sec.ms

--- commands/db/edit-submissions.py
+++ commands/db/edit-submissions.py
@@ -12,13 +12,11 @@ class Edit(commands.Cog):
         connection = sqlite3.connect("database/tasks.db")
         cursor = connection.cursor()
 
-        # TODO: Instead of seconds, this format would be preferable: 1:23.456 instead. It allows for better reading of minutes
-        cursor.execute(f"UPDATE submissions SET time = {time}, dq = {dq} WHERE id = {user.id}")
-        connection.commit() 
-        connection.close() 
+        # Update submission
+        cursor.execute("UPDATE submissions SET time = ?, dq = ? WHERE id = ?", (time, dq, user.id))
+        connection.commit()
+        connection.close()
         await ctx.reply(f"Edited {user}'s submission.")
-
-        # TODO: Send affected competitor a message saying their time was updated, and if they DQ'ed.
 
 
 async def setup(bot) -> None:
