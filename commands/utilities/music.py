2b6d163c0c03a01b6f8598a7e3256c436c1be920
DashSuperCraft <Megayoshi.remi@gmail.com>
2024-06-06 10:53:39 -0400

    grettings and farewells on leave/join vc (MKW)

--- commands/utilities/music.py
+++ commands/utilities/music.py
@@ -2,17 +2,22 @@ import discord
 from discord.ext import commands
 import yt_dlp
 import asyncio
+import os
+from dotenv import load_dotenv
+import asyncio
 
 queues = {}
 voice_clients = {}
 yt_dl_options = {"format": "bestaudio/best"}
 ytdl = yt_dlp.YoutubeDL(yt_dl_options)
 
-ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-                      'options': '-vn -filter:a "volume=0.25"'}
+ffmpeg_options = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn -filter:a "volume=0.25"'
+}
 
-ffmpeg_path = './ffmpeg/ffmpeg.exe'
-
+load_dotenv()
+DEFAULT = os.getenv('DEFAULT')
 
 class Music(commands.Cog):
     def __init__(self, bot) -> None:
@@ -22,9 +27,23 @@ class Music(commands.Cog):
     async def joinvc(self, ctx):
         if ctx.author.voice:
             channel = ctx.author.voice.channel
-            if ctx.voice_client is not None:
-                return await ctx.voice_client.move_to(channel)
-            await channel.connect()
+
+            # Check if the bot is already in a voice channel
+            if ctx.voice_client:
+                # Move the bot to the new channel
+                await ctx.voice_client.disconnect()
+                await channel.connect()
+            else:
+                # Connect the bot to the channel
+                await channel.connect()
+
+            # Reference the voice client explicitly
+            vc = ctx.voice_client
+
+            # Play the sound file
+            if DEFAULT == 'mkw':
+                source = discord.FFmpegPCMAudio('./commands/utilities/mkwii/on_join.wav')
+                vc.play(source)
         else:
             await ctx.send("You are not connected to a voice channel.")
 
@@ -34,7 +53,7 @@ class Music(commands.Cog):
             if ctx.voice_client is not None:
                 voice_client = ctx.voice_client
             else:
-                voice_client = await ctx.author.voice.channel.connect()
+                return await ctx.send("I am not in a voice chat! Invite me by doing `$joinvc`.")
 
             voice_clients[ctx.guild.id] = voice_client
 
@@ -42,7 +61,7 @@ class Music(commands.Cog):
             data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
 
             song = data['url']
-            player = discord.FFmpegPCMAudio(song, executable=ffmpeg_path, **ffmpeg_options)
+            player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
 
             if not voice_clients[ctx.guild.id].is_playing():
                 voice_clients[ctx.guild.id].play(player, after=lambda e: print('Player error: %s' % e) if e else None)
@@ -52,9 +71,16 @@ class Music(commands.Cog):
         except Exception as e:
             print(f'Error: {e}')
 
-    @commands.hybrid_command(name="leave", description="Kick the bot ouf of your VC channel.", with_app_command=True)
+    @commands.hybrid_command(name="leave", description="Kick the bot out of your VC channel.", with_app_command=True)
     async def leave(self, ctx):
         if ctx.voice_client:
+
+            if DEFAULT == 'mkw':
+                # Funky kong sends his farewells
+                source = discord.FFmpegPCMAudio('./commands/utilities/mkwii/on_leave.wav')
+                ctx.voice_client.play(source)
+                await asyncio.sleep(7)
+
             await ctx.voice_client.disconnect()
             voice_clients.pop(ctx.guild.id, None)
             await ctx.send("Disconnected from the voice channel.")
