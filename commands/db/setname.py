7e13554e625991cf068831a1cdf751854c24adf8
DashSuperCraft <Megayoshi.remi@gmail.com>
2024-06-11 19:55:38 -0400

    Fixed setname schenanigans

--- commands/db/setname.py
+++ commands/db/setname.py
@@ -16,16 +16,22 @@ async def rename_in_submission_list(self, old_display_name, new_display_name):
         # Check if the message was sent by the bot
         if message.author == self.bot.user:
             lines = message.content.split('\n')
-            new_lines = []
-            for line in lines:
+            new_lines = [lines[0]]  # Preserve the first line as is ("Current submissions")
+
+            # Check if the new display name is already in use in the rest of the lines
+            for line in lines[1:]:
+                if new_display_name in line:
+                    return
+
+            for line in lines[1:]:
                 if old_display_name in line:
                     line = line.replace(old_display_name, new_display_name)
                 new_lines.append(line)
+
             new_content = '\n'.join(new_lines)
             if new_content != message.content:
                 await message.edit(content=new_content)
             break  # Stop after finding the last bot message
-
 
 class Setname(commands.Cog):
     def __init__(self, bot) -> None:
@@ -39,11 +45,19 @@ class Setname(commands.Cog):
         if '@' in new_name:
             return await ctx.reply("You may not use @ in your name.")
 
+        if len(new_name) > 120:
+            return await ctx.reply("Your name is too long!")
+
         try:
             # retrieve old name
             cursor.execute("SELECT * from userbase WHERE id = ?", (ctx.author.id,))
             result = cursor.fetchone()
             old_display_name = result[2]
+
+            # Check if the new name is already in use in the database
+            cursor.execute("SELECT * from userbase WHERE display_name = ?", (new_name,))
+            if cursor.fetchone():
+                return await ctx.reply("The name is already in use by another user.")
 
             # try to update
             cursor.execute("UPDATE userbase SET display_name = ? WHERE id = ?", (new_name, ctx.author.id))
@@ -62,7 +76,6 @@ class Setname(commands.Cog):
             await ctx.send("Error occured. Maybe try submitting, then retrying?")
         finally:
             connection.close()
-
 
 async def setup(bot) -> None:
     await bot.add_cog(Setname(bot))
