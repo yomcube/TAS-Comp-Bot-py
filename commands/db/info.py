a6dfee058cbebb53decda14f32f0cafbaf7dafcb
shxd <paolaccialesssndro@gmail.com>
2024-06-19 14:24:23 +0200

    tried info stuff

--- commands/db/info.py
+++ commands/db/info.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
-from api.utils import float_to_readable, session
+from api.utils import float_to_readable, download_from_url, session
 from api.db_classes import Submissions
 from sqlalchemy import select
+from api.mkwii.mkwii_utils import get_character, get_controller_type, get_lap_time, get_track, get_vehicle
 
 
 class Info(commands.Cog):
@@ -14,7 +15,7 @@ class Info(commands.Cog):
     async def info(self, ctx):
 
         # Get submission
-        submission = session.execute(select(Submissions.task, Submissions.url, Submissions.time, Submissions.dq,
+        submission = await session.execute(select(Submissions.task, Submissions.url, Submissions.time, Submissions.dq,
                                             Submissions.dq_reason).where(Submissions.user_id == ctx.author.id)).fetchone()
 
         if not submission:
@@ -24,7 +25,12 @@ class Info(commands.Cog):
         # variables for all the columns
         task_num = submission[0]
         url = submission[1]
+        rkg = await download_from_url(url)
         time = float_to_readable(submission[2])
+        track = get_track(rkg)
+        character = get_character(rkg)
+        vehicle = get_vehicle(rkg)
+        controller = get_controller_type(rkg)
         dq = bool(submission[3])
         dq_reason = submission[4]
 
@@ -32,6 +38,10 @@ class Info(commands.Cog):
 
         embed.add_field(name="File", value=f"{url}", inline=True)
         embed.add_field(name="Time", value=f"{time}", inline=True)
+        embed.add_field(name="Track", value=f"{track}", inline=True)
+        embed.add_field(name="Character", value=f"{character}", inline=True)
+        embed.add_field(name="Vehicle", value=f"{vehicle}", inline=True)
+        embed.add_field(name="Controller", value=f"{controller}", inline=True)
         embed.add_field(name="DQ", value=f"{dq}", inline=True)
         if dq:
             embed.add_field(name="DQ reason", value=dq_reason, inline=True)
