966ed9ddadb132add331b141eb6cbd3d8fc71580
DashQC <Megayoshi.remi@gmail.com>
2025-02-01 22:39:11 -0500

    commit

--- api/submissions.py
+++ api/submissions.py
@@ -134,17 +134,6 @@ async def post_submission_list(channel, id, name):
         team_name = await get_team_name(id)
         mentions = ' '.join([f'<@{user_id}>' for user_id in ids])
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        return await channel.send(
-            f"**__Current Submissions:__**\n1. {team_name} ({' & '.join(members)}) ||{mentions}||",
-            allowed_mentions=discord.AllowedMentions.none(), suppress_embeds=True)
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         # No ( ) if no team name
         if team_name == None:
             return await channel.send(
@@ -155,13 +144,6 @@ async def post_submission_list(channel, id, name):
             return await channel.send(
                 f"**__Current Submissions:__**\n1. {team_name} ({' & '.join(members)}) ||{mentions}||",
                 allowed_mentions=discord.AllowedMentions.none(), suppress_embeds=True)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     # Case if solo
     return await channel.send(
