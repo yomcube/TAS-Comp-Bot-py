5ae820204f5667a47f662a1ffcde33b62a38081b
TomCube <121310847+yomcube@users.noreply.github.com>
2025-04-08 15:36:09 -0500

    "DM handling" -> "file handling"

--- api/file_handlers.py
+++ api/file_handlers.py
@@ -2,9 +2,9 @@ from api.mkwii.mkwii_file_handling import handle_mkwii_files
 from api.nsmbwii.nsmbwii_file_handling import handle_nsmbwii_files
 
 handlers_dict = {}
-def add_dm_handler(comp, func):
+def add_file_handler(comp, func):
     handlers_dict[comp] = func
 
-def init_dm_handlers():
-    add_dm_handler("mkw", handle_mkwii_files)
-    add_dm_handler("nsmbw", handle_nsmbwii_files)
+def init_file_handlers():
+    add_file_handler("mkw", handle_mkwii_files)
+    add_file_handler("nsmbw", handle_nsmbwii_files)
