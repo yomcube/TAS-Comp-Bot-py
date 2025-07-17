12f232b32b1cb3c8afe28560f956a8152e3e4d05
crackhex <troike@arizona.edu>
2025-07-17 16:05:26 -0700

    pull out hostdissolve functionality from discord, move a function

--- commands/db/team/hostdissolve.py
+++ commands/db/team/hostdissolve.py
@@ -1,22 +1,7 @@
 from discord.ext import commands
-from sqlalchemy import select, delete
 
-from api.db_classes import Teams, get_session
+from api.task_handling import dissolve_team
 from api.utils import has_host_role
-
-
-async def reorder_primary_keys():
-    async with get_session() as session:
-        # Retrieve the teams
-        query = select(Teams).order_by(Teams.index)
-        result = await session.execute(query)
-        teams = result.scalars().all()
-
-        # Reassign indexes
-        for idx, team in enumerate(teams, start=1):
-            team.index = idx
-
-        await session.commit()
 
 
 class HostDissolve(commands.Cog):
@@ -27,19 +12,8 @@ class HostDissolve(commands.Cog):
                              description="Forcefully disband a team (WARNING: No confirmation)", with_app_command=True)
     @has_host_role()
     async def hostdissolve(self, ctx, index: int):
-        async with get_session() as session:
-            teamlist = (await session.scalars(select(Teams))).fetchall()
-            total_teams = len(teamlist)
-
-            if index < 0 or index > total_teams:
-                return await ctx.send("Invalid team number entered. See $teams")
-
-            await session.execute(delete(Teams).where(Teams.index == index))
-            await session.commit()
-
-            await reorder_primary_keys()
-
-            await ctx.send("The team has been deleted.")
+        await dissolve_team(index)
+        await ctx.send("The team has been deleted.")
 
 
 async def setup(bot):
